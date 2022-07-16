97fe2dd7a1b064bbb072d9883e8280471559bb72
Sancho Júnior <sancho.junior@vicegov.ce.gov.br>
2022-07-16 15:16:38 -0300

    Attributes_for part2

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -29,9 +29,10 @@ RSpec.describe Customer, type: :model do
     expect(customer.full_name).to start_with("Sr. ")
   end
 
-  it 'Usando' do
+  it 'Usando o attributes_for' do
     attrs = attributes_for(:customer)
-    puts attrs
+    customer = Customer.create(attrs)
+    expect(customer.full_name).to start_with("Sr. ")
   end
 
   it { expect{ create(:customer)}.to change {Customer.all.size}.by(1) }
