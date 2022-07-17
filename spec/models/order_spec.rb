0a1cd80e74646de225a482a78be63c157f190e31
Sancho Jr <sanchojunior.ce@gmail.com>
2022-07-17 16:55:08 -0300

    Create List

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Order, type: :model do
     puts order.customer.name
     expect(order.customer).to be_kind_of(Customer)
   end  
+
+  it 'Tem 3 pedidos' do
+    orders = create_list(:order, 3)
+    expect(orders.count).to eq(3)
+  end  
 end
