f6a688435c9a1c9c6e0438bdad7c6eadcdf41045
Sancho Jr <sanchojunior.ce@gmail.com>
2022-07-19 07:45:09 -0300

    Webmock

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,9 +1,11 @@
 require 'httparty'
 
 describe 'HTTParty' do
-    it 'HTTParty' do
-        response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-        content_type = response.headers['content-type']
-        expect(content_type).to match(/application\/json/)
+    it 'content-type' do
+        stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+         to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
+        # response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+        # content_type = response.headers['content-type']
+        # expect(content_type).to match(/application\/json/)
     end    
 end    
