776dfc8d9d3c8db656357c1346fd7082703cc628
Sancho Jr <sanchojunior.ce@gmail.com>
2022-08-02 06:10:07 -0300

    Testando Controllers (entradas do usuário)

--- spec/controllers/customer_controller_spec.rb
+++ spec/controllers/customer_controller_spec.rb
@@ -23,23 +23,35 @@ RSpec.describe CustomersController, type: :controller do
     end    
 
     describe 'as Logged Member' do
+        before do
+            @member = create(:member)
+            @customer = create(:customer)
+        end
+
+        it 'with valid attributes' do
+            customer_params = attributes_for(:customer)
+            sign_in @member
+
+            expect {
+                post :create, params: { customer: customer_params }
+            }.to change(Customer, :count).by(1)
+
+            p customer_params
+        end
+
         it 'responds a 200 response' do
-            member = create(:member)
-            customer = create(:customer)
-    
-            sign_in member
-    
-            get :show, params: { id: customer.id }
+            #member = create(:member)
+            #customer = create(:customer)
+            sign_in @member
+            get :show, params: { id: @customer.id }
             expect(response).to have_http_status(200)
         end 
 
         it 'render a show template' do
-            member = create(:member)
-            customer = create(:customer)
-    
-            sign_in member
-    
-            get :show, params: { id: customer.id }
+            #member = create(:member)
+            #customer = create(:customer)
+            sign_in @member
+            get :show, params: { id: @customer.id }
             expect(response).to render_template(:show)
         end 
     end
