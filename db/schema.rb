0d400d55831ea3198dedefe4868c4aec22adce97
Sancho Júnior <sanchorodriguesnovooriente@gmail.com>
2022-08-11 09:32:22 -0300

    Ajax

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20220730235542) do
     t.string "email"
     t.boolean "vip"
     t.integer "days_to_pay"
+    t.string "gender"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,7 @@ end
 RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include Warden::Test::Helpers
 
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
