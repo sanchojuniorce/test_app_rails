101fc2af256f2d921dedebf20cae0d6d49c6b2b5
Sancho Jr <sanchojunior.ce@gmail.com>
2022-08-13 07:27:59 -0300

    93. Matcher include_json

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
     factory :customer do
-        name "Jackson Pires"
-        email "jackson@pires.com"
+        name { "Jackson Pires" }
+        email { "jackson@pires.com" }
     end
 end
